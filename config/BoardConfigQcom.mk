476974ede7cb7f988250febaa43b549a010a4e71
BlackDeaths <blackjubels@gmail.com>
2019-09-27 19:55:37 +0200

    Revert "build: Remove TARGET_CRYPTFS_HW_PATH"
    This reverts commit fd14cf08854af3fab83e8ddb0b98709096fa8454.

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -93,5 +93,9 @@ else
 endif
 
 PRODUCT_SOONG_NAMESPACES += \
-    hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT) \
-    vendor/qcom/opensource/audio-hal/$(QCOM_HARDWARE_VARIANT)
+    hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
